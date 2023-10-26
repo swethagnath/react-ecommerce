896922ab9d336e775f0d4363c5cf8f9d59ce4aff
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-10-26 11:00:28 +0530

    added description

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom'
 import Shop from './Shop'
 import Breadcrums  from '../components/BreadCrums/Breadcrums'
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
 
 export const Product = () => {
     const {all_product} = useContext(ShopContext)
@@ -14,6 +15,7 @@ export const Product = () => {
         <div>
             <Breadcrums product={product} />
             <ProductDisplay product={product} />
+            <DescriptionBox/>
         </div>
     )
 }
--- src/components/DescriptionBox/DescriptionBox.jsx
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './DescriptionBox.css'
 
-export const descriptionBox = () => {
+const descriptionBox = () => {
     return (
         <div className="descriptionBox">
-            
+            <div className="descriptionbox-naviagtor">
+                <div className="descriptionbox-nav-box">
+                    Description
+                </div>
+                <div className="descriptionbox-nav-box fade">
+                    Reviews(122)
+                </div>
+                <div className="descriptionbox-description">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores enim est tenetur adipisci esse? Eum maiores laudantium molestiae at aperiam quod necessitatibus dolorem corrupti ipsum facere. Temporibus tempore, inventore debitis dicta laborum voluptatibus laboriosam alias repellat facilis ratione optio tenetur magnam corporis dolorem. Nulla.</p>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default descriptionBox
