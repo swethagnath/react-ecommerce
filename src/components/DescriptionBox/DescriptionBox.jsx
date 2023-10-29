68e98c7dc411e43e9c8aa0b04b57ec73b9bd785e
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2023-10-29 20:48:15 +0530

    added feature description box

--- src/components/DescriptionBox/DescriptionBox.jsx
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -3,7 +3,7 @@ import './DescriptionBox.css'
 
 const descriptionBox = () => {
     return (
-        <div className="descriptionBox">
+        <div className="descriptionbox">
             <div className="descriptionbox-naviagtor">
                 <div className="descriptionbox-nav-box">
                     Description
@@ -11,9 +11,10 @@ const descriptionBox = () => {
                 <div className="descriptionbox-nav-box fade">
                     Reviews(122)
                 </div>
-                <div className="descriptionbox-description">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores enim est tenetur adipisci esse? Eum maiores laudantium molestiae at aperiam quod necessitatibus dolorem corrupti ipsum facere. Temporibus tempore, inventore debitis dicta laborum voluptatibus laboriosam alias repellat facilis ratione optio tenetur magnam corporis dolorem. Nulla.</p>
-                </div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maiores enim est tenetur adipisci esse? Eum maiores laudantium molestiae at aperiam quod necessitatibus dolorem corrupti ipsum facere. Temporibus tempore, inventore debitis dicta laborum voluptatibus laboriosam alias repellat facilis ratione optio tenetur magnam corporis dolorem. Nulla.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, maxime quis ut nemo quos sapiente, iure ipsum laborum quas, dolorem recusandae reprehenderit corrupti voluptatibus expedita officia animi fugit eum porro iste doloribus molestiae? Accusamus necessitatibus, ab fugit magni impedit ad tempore, nulla nam animi aliquam cupiditate. Tempore, aut rem eveniet beatae, velit iusto aliquid saepe maiores molestias ea pariatur dignissimos quaerat. Velit, quisquam aliquid fugiat esse fuga illum iusto at rem inventore id earum incidunt ipsam ut doloribus distinctio et provident ad ea nisi dolore ducimus dignissimos vero! Et accusamus vel dolores cum nam saepe illum ut porro nobis pariatur?</p>
             </div>
         </div>
     )
